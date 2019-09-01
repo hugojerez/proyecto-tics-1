a54f353ef480fb211e51396488bf9adf01f702ef
Hugo <hugojerez21@gmail.com>
2019-08-31 20:33:33 -0400

    a

---
+++ tics/vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+	// options...
+	baseUrl: "https://raw.githack.com/hugojerez/proyecto-tics-1/master/tics/dist/",
+	publicPath: "https://raw.githack.com/hugojerez/proyecto-tics-1/master/tics/dist/"
+}
